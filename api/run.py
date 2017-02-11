e7f4286462aad18b4d2734bc29afa8a8792a4bd5
Ubuntu <jarvismesper@jarvis-mesper-api.jarvis-mesper-api.a9.internal.cloudapp.net>
2017-02-11 02:09:54 +0000

    change port

--- api/run.py
+++ api/run.py
@@ -34,6 +34,7 @@ def getingredients(code):
 
 @app.route("/getbarcode/<string:code>")
 def get_barcode(code):
+    print("Getting barcode" + code)
     try:
         res = RequestOpenFood.get_product(barcode=code)
         res = ProductBuilder.clean_data(res)
@@ -42,5 +43,5 @@ def get_barcode(code):
         return jsonify(data=[])
  
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0')
 
